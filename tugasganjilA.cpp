9be5b5962bfeed1bdcd0d93c998ed2269396155f
maulinakhamidah <maulinakhamidah115@gmail.com>
2025-05-17 18:42:07 +0700

    tugasganjil

--- tugasganjilA.cpp
+++ tugasganjilA.cpp
@@ -1,4 +1,53 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class mahasiswa
+class nama;
+class nim;
+class nilai;
+
+class mahasiswa{
+    private:
+    string nama;
+    string nim;
+    float nilai;
+
+    void displayData()
+    cout << "mahasiswa: " << nama << ", nim: " << nim << ", nilai: " << nilai << endl;
+
+    friend class dosen;
+};
+
+class dosen{
+    private:
+    string nama;
+    string NIDN;
+    string pangkat;
+    float gaji;
+
+    void displayData()
+    cout << "dosen: " << nama << ", NIDN: " << NIDN << ", pangkat: " << pangkat << ", gaji: " << gaji << endl;
+};
+
+class staff{
+    private:
+    string nama;
+    string IDstaff;
+    float gaji;
+
+    void displayData()
+    cout << "staff" << nama << ",  IDstaff: " << IDstaff << ", gaji: " << gaji << endl;
+};
+
+int main()
+{
+    mahasiswa mhs("Ani")
+    dosen dsn("Dr. wira")
+    staff stff("udin")
+
+    mhs.tampilkan();
+    dsn.tampilkan();
+    stf.tampilkan();
+
+    return 0;
+}
